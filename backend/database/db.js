6b37612026dc54373c81df6c71749ce0d2e856f4
Domján Réka <48532960+rekad97@users.noreply.github.com>
2021-12-02 20:38:34 +0100

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const config = require('../config/config.json');
 const mongoose = require('mongoose');
 
 const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connect(process.env.MONGODB_URI, connectionOptions);
 mongoose.Promise = global.Promise;
 
 module.exports = {
